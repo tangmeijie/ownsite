be11acfb14b72697aa0a9493464429e6de710acb
jayla <540745852@qq.com>
2021-09-22 15:06:23 +0800

    test sth

--- website.code/src/js/index.js
+++ website.code/src/js/index.js
@@ -2,11 +2,9 @@ import "../styles/a.scss"
 import anime from 'animejs/lib/anime.es.js';
 
 // console.log(moment().format('YYYY-MM-DD'))
-anime({
-  targets: 'h1',
-  translateX: 250,
-  // rotate: '1turn',
-  // backgroundColor: '#FFF',
-  duration: 800,
-  loop: true
-});
+// anime({
+//   targets: 'h1',
+//   translateX: 250,
+//   duration: 800,
+//   loop: true
+// });
