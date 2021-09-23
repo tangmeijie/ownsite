cac28b6920582d503441191774556256b8a31f40
jayla <540745852@qq.com>
2021-09-23 12:56:30 +0800

    sass更换成less

--- website.code/src/js/index.js
+++ website.code/src/js/index.js
@@ -1,10 +1,10 @@
-import "../styles/a.scss"
+import "../styles/b.less"
 import anime from 'animejs/lib/anime.es.js';
 
 // console.log(moment().format('YYYY-MM-DD'))
-// anime({
-//   targets: 'h1',
-//   translateX: 250,
-//   duration: 800,
-//   loop: true
-// });
+anime({
+  targets: 'h1',
+  translateX: 250,
+  duration: 800,
+  loop: true
+});
