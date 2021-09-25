dd2b7082777b7eb70be9658efb31dfde57bf7b90
jayla <540745852@qq.com>
2021-09-25 21:28:47 +0800

    简单搭了一下网站框架

--- website.code/app.js
+++ website.code/app.js
@@ -2,7 +2,19 @@ module.exports = {
   index: {
     html: './src/html/index.html',
     js: [
-        "./src/js/index.js"
+      './src/js/main.js'
+    ]
+  },
+  about-me: {
+    html: './src/html/about-me.html',
+    js: [
+      './src/js/main.js'
+    ]
+  },
+  fengos: {
+    html: './src/html/fengos.html',
+    js: [
+      './src/js/main.js'
     ]
   }
 }
