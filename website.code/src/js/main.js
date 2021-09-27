fab48a1dea198281ebef4e72229b9f50dd7b1169
jayla <540745852@qq.com>
2021-09-27 14:46:48 +0800

    将样式分离，建立公用样式变量库

--- website.code/src/js/main.js
+++ website.code/src/js/main.js
@@ -1,5 +1,4 @@
-import "../styles/clear.scss"
-import "../styles/color.scss"
+import '../styles/home.scss'
 import anime from 'animejs/lib/anime.es.js';
 
 // console.log(moment().format('YYYY-MM-DD'))
