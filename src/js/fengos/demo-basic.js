78ec10106fb4059abc4e0969366fef8b34ff0eb5
jayla <540745852@qq.com>
2021-10-27 18:54:38 +0800

    [FOS] 修改demo公用函数

--- src/js/fengos/demo-basic.js
+++ src/js/fengos/demo-basic.js
@@ -3,25 +3,6 @@ export {
   fnNodeIndex
 }
 
-function fnKeytoStr(keycode) {
-  switch (keycode) {
-    case 37:
-      return 'left'
-    case 38:
-      return 'up'
-    case 39:
-      return 'right'
-    case 40:
-      return 'down'
-    case 13:
-      return 'enter' // 回车
-    case 32:
-      return 'back' // 空格
-    default:
-      return false
-  }
-}
-
 function fnGetTime() {
   let d = new Date()
   let h = d.getHours()
@@ -36,6 +17,25 @@ function fnGetTime() {
   return h + ':' + m
 }
 
+function fnKeytoStr(keycode) {
+  switch (keycode) {
+    case 37:
+      return 'left'
+    case 38:
+      return 'up'
+    case 39:
+      return 'right'
+    case 40:
+      return 'down'
+    case 13:
+      return 'ok' // 回车
+    case 32:
+      return 'back' // 空格
+    default:
+      return false
+  }
+}
+
 function fnNodeIndex(nodelist, node) {
   let array = Array.from(nodelist)
   let index = array.indexOf(node)
@@ -43,5 +43,16 @@ function fnNodeIndex(nodelist, node) {
     return index
   } else {
     return false
+  }
+}
+
+function fnToggleFocus(oNew, fn) {
+  let oOld = document.getElementById('focus')
+  oOld.id = ''
+  oOld.classList.remove('mark')
+  oNew.id = 'focus'
+  oNew.classList.add('mark')
+  if (fn) {
+    fn(oOld, oNew)
   }
 }
--- src/js/fengos/demo-desktop.js
+++ src/js/fengos/demo-desktop.js
@@ -4,15 +4,16 @@ import {
   fnGetTime,
   fnNodeIndex
 } from '../../js/fengos/demo-basic.js'
+
 import anime from 'animejs/lib/anime.es.js'
 
-// 状态栏时间走动
+// 状态栏时间
 const oTime = document.getElementById('time')
 oTime.innerHTML = fnGetTime()
 setInterval(function () {
   oTime.innerHTML = fnGetTime()
 }, 1000 * 30)
 
-
+// 频道栏切换
 const oChannel = document.getElementById('channel')
 const aChannelCells = oChannel.getElementsByClassName('cell')
