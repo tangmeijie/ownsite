e4db7069a4d72e9cca9bf4d4e28d63bb2278c09e
jayla <540745852@qq.com>
2021-10-31 19:08:12 +0800

    [FOS] 使demo界面尺寸适应浏览器显示尺寸

--- src/js/fengos/demo-basic.js
+++ src/js/fengos/demo-basic.js
@@ -1,7 +1,32 @@
 export {
   fnGetTime,
   fnNodeIndex,
-  fnActions
+  fnMarkFocus,
+  fnToggleFocus
+}
+
+fnFitScreen()
+window.onresize = fnFitScreen
+
+function fnFitScreen() {
+  const iScreenWidth = document.body.clientWidth
+  const iScreenHeight = document.body.clientHeight
+
+  const iPageWidth = 1920
+  const iPageHeight = 1080
+
+  const iWidth = iScreenWidth / iPageWidth
+  const iHeight = iScreenHeight / iPageHeight
+
+  let iScale
+  if (iWidth < iHeight) {
+    iScale = iWidth
+  } else {
+    iScale = iHeight
+  }
+
+  const oPage = document.getElementById('desktop')
+  oPage.style.transform = 'scale(' + iScale + ')'
 }
 
 function fnGetTime() {
@@ -18,36 +43,6 @@ function fnGetTime() {
   return h + ':' + m
 }
 
-function fnActions() {
-  document.addEventListener("keydown", function (event) {
-    
-    console.log(event.code)
-
-    switch (event.code) {
-
-      case 'ArrowRight':
-        break
-
-      case 'ArrowLeft':
-        break
-
-      case 'ArrowDown':
-        break
-
-      case 'ArrowUp':
-        break
-
-      case 'Enter':
-        break
-
-      case 'Space':
-        break
-
-    }
-
-  })
-}
-
 function fnNodeIndex(nodelist, node) {
   const array = Array.from(nodelist)
   const index = array.indexOf(node)
@@ -55,5 +50,52 @@ function fnNodeIndex(nodelist, node) {
     return index
   } else {
     return false
+  }
+}
+
+function fnMarkFocus(oBox, sSelector) {
+  let oFocus
+  if (oBox.querySelector('.mark')) {
+    oFocus = oBox.querySelector('.mark')
+  } else {
+    oFocus = oBox.querySelector(sSelector)
+    oFocus.classList.add('mark')
+  }
+  oFocus.id = 'focus'
+}
+
+function fnToggleFocus(oFocusNew) {
+  let oFocus = document.getElementById('focus')
+  oFocus.id = ''
+  oFocus.classList.remove('mark')
+
+  oFocusNew.id = 'focus'
+  oFocus.classList.add('mark')
+}
+
+function fnAddAction(event) {
+
+  console.log(event.code)
+
+  switch (event.code) {
+
+    case 'ArrowRight':
+      break
+
+    case 'ArrowLeft':
+      break
+
+    case 'ArrowDown':
+      break
+
+    case 'ArrowUp':
+      break
+
+    case 'Enter':
+      break
+
+    case 'Space':
+      break
+
   }
 }
