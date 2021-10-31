0c22a3a67fb0458cd81dc6b71a843f70ddf112f3
jayla <540745852@qq.com>
2021-10-31 21:09:23 +0800

    [FOS] 调整焦点函数，频道栏滑动动画

--- src/js/fengos/demo-desktop.js
+++ src/js/fengos/demo-desktop.js
@@ -3,7 +3,8 @@ import '../../styles/fengos/demo-desktop.scss'
 import {
   fnGetTime,
   fnNodeIndex,
-  fnActions
+  fnMarkFocus,
+  fnToggleFocus
 } from '../../js/fengos/demo-basic.js'
 
 import anime from 'animejs/lib/anime.es.js'
@@ -17,26 +18,68 @@ setInterval(function () {
 
 // 频道栏切换
 const oChannel = document.getElementById('channel')
+fnChannelFocus()
 
-function fnToggleChannel(direction) {
-  const aChannelCells = oChannel.getElementsByClassName('cell')
+function fnChannelFocus() {
+  fnMarkFocus(oChannel, '.cell')
+  oChannel.classList.add('highlight')
+
   let oFocus = document.getElementById('focus')
-  let index = fnNodeIndex(aChannelCells, oFocus)
+  fnChannelSilder(oFocus)
 
-  switch (direction) {
-    case 'right':
-      if (index < aChannelCells.length) {
-        oFocus.id = ''
-        aChannelCells[index + 1].id = 'focus'
-      }
-      break
-    case 'left':
-      if (index > 0) {
-        oFocus.id = ''
-        aChannelCells[index - 1].id = 'focus'
-      }
-      break
-  }
+  fnToggleChannel(oChannel)
 }
 
-fnActions()
+function fnChannelBlur() {
+  let oFocus = document.getElementById('focus')
+  oFocus.id = ''
+
+  oChannel.classList.remove('highlight')
+}
+
+function fnToggleChannel(oBox) {
+  const aCells = oBox.getElementsByClassName('cell')
+
+  document.addEventListener('keydown', function (event) {
+    let oFocusOld = document.getElementById('focus')
+    let index = fnNodeIndex(aCells, oFocusOld)
+
+    switch (event.code) {
+
+      case 'ArrowRight':
+        if (index < aCells.length - 1) {
+          fnToggleFocus(aCells[index + 1])
+          fnChannelSilder(aCells[index + 1])
+        }
+        break
+
+      case 'ArrowLeft':
+        if (index > 0) {
+          fnToggleFocus(aCells[index - 1])
+          fnChannelSilder(aCells[index - 1])
+        }
+        break
+
+      case 'ArrowDown':
+        // fnChannelBlur()
+        break
+    }
+  })
+}
+
+function fnChannelSilder(oFocusNew) {
+  const oSilder = document.getElementById('channel-slider')
+  const oBar = oSilder.parentNode
+
+  const iLeft = oFocusNew.offsetLeft
+  const iRight = oBar.offsetWidth - iLeft - oFocusNew.offsetWidth
+
+  let sliderAnime = anime({
+    targets: oSilder,
+    left: iLeft,
+    right: iRight,
+    easing: 'spring(0.5, 100, 36, 24)',
+    autoplay: false
+  })
+  sliderAnime.play()
+}
