f42f644f1e5639e416442e3f4aa9ae9859d78d4d
jayla <540745852@qq.com>
2021-11-10 19:10:28 +0800

    [FOS] 新增demo的数据文件

---
+++ src/js/fengos/demo-data.js
@@ -0,0 +1,26 @@
+aMovies = [{
+  id: 101,
+  name: '',
+  director: '',
+  cast: '',
+  country: '',
+  date: '',
+  duration: '',
+  type: '',
+  intro: '',
+  recommend: '',
+  assets: {
+    poster: '',
+    title: '',
+    cover: '',
+    coverTitle: '',
+    trailer: ''
+  },
+  awards: []
+}]
+
+aCasts = [{
+  id: 001,
+  name: '',
+  nameEn: ''
+}]
