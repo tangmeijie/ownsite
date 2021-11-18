6c7b278e5751e7e39cb9b2533b61ad2225b0f430
jayla <540745852@qq.com>
2021-11-19 00:25:16 +0800

    [FOS] 修复在零点时只显示一位数字

--- src/js/fengos/demo-basic.js
+++ src/js/fengos/demo-basic.js
@@ -49,11 +49,11 @@ function fnGetTime() {
   let h = d.getHours()
   let m = d.getMinutes()
   let s = d.getSeconds()
+  if (h < 10) {
+    h = '0' + h
+  }
   if (m < 10) {
     m = '0' + m
-  }
-  if (s < 10) {
-    s = '0' + s
   }
   return h + ':' + m
 }
