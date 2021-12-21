2b08b59b3cc17998053ca414297af78820bb14d9
jayla <540745852@qq.com>
2021-12-21 23:31:18 +0800

    完成遥控器界面

--- src/js/fengos/controller.js
+++ src/js/fengos/controller.js
@@ -1,1 +1,10 @@
 import '../../styles/fengos/controller.scss'
+
+;
+(function fnControllDemo() {
+  const oBack = document.getElementById('back')
+
+  oBack.onclick = function () {
+    parent.window.document.getElementById('channel-slider').style.background = 'red'
+  }
+})()
