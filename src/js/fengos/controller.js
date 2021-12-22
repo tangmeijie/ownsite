a0a1b3e3baa390e84610c55403ff2bc9e5efa0b2
jayla <540745852@qq.com>
2021-12-22 23:45:50 +0800

    遥控器探索

--- src/js/fengos/controller.js
+++ src/js/fengos/controller.js
@@ -1,5 +1,11 @@
 import '../../styles/fengos/controller.scss'
 
+// 避免遥控器获得焦点
+window.onmousedown = function (e) {
+  e.preventDefault()
+  return false
+}
+
 ;
 (function fnControllDemo() {
   const oBack = document.getElementById('back')
@@ -8,3 +14,6 @@ import '../../styles/fengos/controller.scss'
     parent.window.document.getElementById('channel-slider').style.background = 'red'
   }
 })()
+
+let webSocket
+webSocket = new WebSocket('ws://localhost:8080')
