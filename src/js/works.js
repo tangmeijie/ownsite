8d938b17d95a46c7653e5aa6729067ef9c383260
jayla <540745852@qq.com>
2024-06-05 12:34:15 +0800

    【新增】使用组件化的方式，新增作品展示页的各项目

--- src/js/works.js
+++ src/js/works.js
@@ -5,4 +5,31 @@ import {
 
 document.addEventListener('DOMContentLoaded', () => {
   fnLoadComponent('/kits/nav.html', 'nav', 'header', 'works')
+  fnLoadProjects()
 })
+
+// 载入项目模板
+function fnLoadProjects() {
+
+  // 获取嵌入在HTML中的项目数据
+  const oProjectsDataElem = document.getElementById('data-project')
+  const projects = JSON.parse(oProjectsDataElem.textContent)
+
+  // 获取模板和目标容器
+  const template = document.getElementById('temp-project').content
+  const oContainer = document.getElementById('container')
+
+  // 渲染项目组件
+  projects.forEach(project => {
+    // 克隆模板内容
+    const clone = document.importNode(template, true);
+
+    // 填充数据
+    clone.querySelector('img').src = project.src;
+    clone.querySelector('h1').textContent = project.title;
+    clone.querySelector('span').textContent = project.role;
+
+    // 添加到容器中
+    oContainer.appendChild(clone);
+  })
+}
