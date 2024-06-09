ef419844e3cd7176168acdb80c039116c7b4fc9d
jayla <540745852@qq.com>
2024-06-09 16:45:43 +0800

    【新增】全局：换上新的网站favicon ，并适配不同主题模式

--- src/js/global.js
+++ src/js/global.js
@@ -3,6 +3,7 @@
 import '../styles/clear.scss'
 
 export {
+  detectTheme,
   fnLoadComponent,
   fnScrollHeader
 }
@@ -18,6 +19,27 @@ export {
 // function isMobile() { 
 //   return ('ontouchstart' in document.documentElement); 
 // }
+
+// 初始化时设置favicon
+setFavicon(detectTheme())
+// 监听系统主题变化并切换favicon
+window.matchMedia('(prefers-color-scheme: dark)').addListener((e) => {
+  setFavicon(e.matches ? 'dark' : 'light')
+})
+
+function setFavicon(theme) {
+  const favicon = document.getElementById('favicon')
+  if (theme === 'dark') {
+    favicon.href = 'favicon-dark.png'
+  } else {
+    favicon.href = 'favicon-light.png'
+  }
+}
+
+function detectTheme() {
+  const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+  return isDarkMode ? 'dark' : 'light'
+}
 
 // 载入组件
 //  @param {string} sKitURL - 组件地址
@@ -55,29 +77,29 @@ function fnScrollHeader() {
   let lastTimestamp = 0
   const headerbar = document.getElementById('header')
 
-  window.addEventListener('scroll', function(event) {
-      let scrollTop = window.pageYOffset || document.documentElement.scrollTop
-      let currentTimestamp = event.timeStamp
-      let timeDifference = currentTimestamp - lastTimestamp
+  window.addEventListener('scroll', function (event) {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+    let currentTimestamp = event.timeStamp
+    let timeDifference = currentTimestamp - lastTimestamp
 
-      // 计算滚动速度
-      let scrollDistance = Math.abs(scrollTop - lastScrollTop)
-      let scrollSpeed = scrollDistance / timeDifference
+    // 计算滚动速度
+    let scrollDistance = Math.abs(scrollTop - lastScrollTop)
+    let scrollSpeed = scrollDistance / timeDifference
 
-      if (scrollTop < 150) {
-          // 滚动后距页面顶部的距离小于该值时，导航栏始终显示
-          headerbar.classList.remove('hide')
-      } else {
-          if (scrollTop > lastScrollTop + 4) {
-              // 向下滚动时隐藏导航栏，需产生一定幅度的距离
-              headerbar.classList.add('hide')
-          } else if (scrollTop < lastScrollTop && scrollSpeed > 0.6) {
-              // 向上滚动且速度（速度单位：px/ms）较快时，显示导航栏
-              headerbar.classList.remove('hide')
-          }
+    if (scrollTop < 150) {
+      // 滚动后距页面顶部的距离小于该值时，导航栏始终显示
+      headerbar.classList.remove('hide')
+    } else {
+      if (scrollTop > lastScrollTop + 4) {
+        // 向下滚动时隐藏导航栏，需产生一定幅度的距离
+        headerbar.classList.add('hide')
+      } else if (scrollTop < lastScrollTop && scrollSpeed > 0.6) {
+        // 向上滚动且速度（速度单位：px/ms）较快时，显示导航栏
+        headerbar.classList.remove('hide')
       }
+    }
 
-      lastScrollTop = scrollTop
-      lastTimestamp = currentTimestamp
+    lastScrollTop = scrollTop
+    lastTimestamp = currentTimestamp
   })
 }
