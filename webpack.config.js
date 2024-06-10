3f2cb094c278126a7f0895f6761994a2726218c2
jayla <540745852@qq.com>
2024-06-11 00:12:27 +0800

    【优化】页脚：增加Magic Space底纹

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
       test: /\.css$/,
       use: ['style-loader', 'css-loader']
     }, {
-      test: /\.(ttf|eof|woff|woff2|svg)/,
+      test: /\.(ttf|eof|woff|woff2|svg|png|jpg|gif)/,
       use: ['file-loader']
     }]
   },
